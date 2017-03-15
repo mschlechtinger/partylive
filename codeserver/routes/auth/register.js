b0e4aa43bdb2f47c50eb1990ce530086da1bdf36
Robert Schulz <robert.schulz-95@web.de>
2017-03-15 16:10:25 +0100

    Login user after successful registration

--- codeserver/routes/auth/register.js
+++ codeserver/routes/auth/register.js
@@ -2,24 +2,30 @@
 
 const express = require('express');
 const router = express.Router();
-const passport = require('passport');
 const Account = require('../../models/account');
 
 
 //handle request for path myserver/auth/register
 router.post('/', function(req, res) {
   console.log('registering user');
-  Account.register(new Account({username: req.body.username}), req.body.password, function(err) {
+  Account.register(new Account({username: req.body.username}), req.body.password, function(err, user) {
     if (err) {
-      console.log('error while user register!', err);
-      res.status(409).json({error: 'User already exists'});
+      console.log(err.name, err.message);
+      var jsonResponse = {};
+      jsonResponse[err.name] = err.message;
+      res.status(409).json(jsonResponse);
+      return;
     }
-
     console.log('user registered!');
+    
+    //Login the newly registered user
+    req.logIn(user, function(err) {
+      if (err) {
+        console.log(err);
+      }
+      console.log('User logged in!');
+    });
     res.status(200).json({"status":"You registered the user " + req.body.username});
-    passport.authenticate('local')(req, res, function () {
-          console.log('User logged in!');
-        });
   });
 });
 
