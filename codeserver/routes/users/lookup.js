e41d3726a9c9d8f1ca708caa78ed1ba56c0ccc12
Robert Schulz <robert.schulz-95@web.de>
2017-04-21 09:38:53 +0200

    user lookup fix

--- codeserver/routes/users/lookup.js
+++ codeserver/routes/users/lookup.js
@@ -10,15 +10,14 @@ const fileHandler =require('../../files/fileHandler');
 router.post('/', authenticationCheck, function(req, res) {
 	UserModel.find({username: req.body.username},'_id name imgUrl', function (err, user) {
 		if(err) return res.status(500).json(err);
+		if(!user[0]) return res.status(404).json({error: "No user found with that name"});
 
-		var response = user;
-		response.imgUrl = fileHandler.getFileUrl(user.imgUrl, req.params.userId, "jpg");
+		var response = user[0];
+		response.imgUrl = fileHandler.getFileUrl(response.imgUrl, req.user.id, "jpg");
 
     	res.status(200).json(response);
 	});
 });
-
-
 
 
 //exports the router as a node module
