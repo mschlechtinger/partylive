b27328c7d43ec30163ea328386acdafd04a53002
kauppfbi <fabian.kaupp@mail.de>
2017-04-22 17:20:28 +0200

    seperated model User and Person again

---
+++ clientapp/app/src/main/java/com/example/d062589/partylive/Models/User.java
@@ -0,0 +1,15 @@
+package com.example.d062589.partylive.Models;
+
+import java.io.Serializable;
+
+/**
+ * Created by kauppfbi on 22.04.2017.
+ */
+
+public class User implements Serializable {
+
+    public String userID;
+    public String name;
+    public String facebookID;
+    public String session;
+}
--- clientapp/app/src/main/java/com/example/d062589/partylive/Utils/PrefUtils.java
+++ clientapp/app/src/main/java/com/example/d062589/partylive/Utils/PrefUtils.java
@@ -2,27 +2,37 @@ package com.example.d062589.partylive.Utils;
 
 import android.content.Context;
 
-import com.example.d062589.partylive.Models.Person;
+import com.example.d062589.partylive.Models.User;
 
 public class PrefUtils {
 
-    public static void setCurrentUser(Person currentUser, Context ctx) {
-        ComplexPreferences complexPreferences = ComplexPreferences.getComplexPreferences(ctx, "user_prefs", 0);
+    private static PrefUtils instance = null;
+    private Context context;
+
+    private PrefUtils(Context context) {
+        this.context = context;
+    }
+
+    public static synchronized PrefUtils getInstance(Context context) {
+        instance = new PrefUtils(context);
+        return instance;
+    }
+
+    public void setCurrentUser(User currentUser) {
+        ComplexPreferences complexPreferences = ComplexPreferences.getComplexPreferences(context, "user_prefs", 0);
         complexPreferences.putObject("current_user_value", currentUser);
         complexPreferences.commit();
     }
 
-    public static Person getCurrentUser(Context ctx) {
-        ComplexPreferences complexPreferences = ComplexPreferences.getComplexPreferences(ctx, "user_prefs", 0);
-        Person currentUser = complexPreferences.getObject("current_user_value", Person.class);
+    public User getCurrentUser() {
+        ComplexPreferences complexPreferences = ComplexPreferences.getComplexPreferences(context, "user_prefs", 0);
+        User currentUser = complexPreferences.getObject("current_user_value", User.class);
         return currentUser;
     }
 
-    public static void clearCurrentUser(Context ctx) {
-        ComplexPreferences complexPreferences = ComplexPreferences.getComplexPreferences(ctx, "user_prefs", 0);
+    public void clearCurrentUser() {
+        ComplexPreferences complexPreferences = ComplexPreferences.getComplexPreferences(context, "user_prefs", 0);
         complexPreferences.clearObject();
         complexPreferences.commit();
     }
-
-
 }
