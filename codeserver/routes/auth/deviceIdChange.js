6acd519481afeb5c7850f05f5f69faa481120980
Florian <bauerflorian95@gmail.com>
2017-04-24 19:42:17 +0200

    latest deviceIdChange (body-params: "userId" : "...", "deviceId" : "...")

--- codeserver/routes/auth/deviceIdChange.js
+++ codeserver/routes/auth/deviceIdChange.js
@@ -4,31 +4,29 @@ const passport = require('passport');
 const express = require('express');
 const router = express.Router();
 const Account = require('../../models/account');
+const authenticationCheck = require('../../authentication/authenticationCheck');
 
 // handle request for path myserver/auth/deviceIdChange
-router.post('/', passport.authenticate('local'), function(req, res){
-	if (req.isAuthenticated()){
+router.post('/', authenticationCheck, function(req, res){
 		
-		//*** save the current deviceId for firebase-PushNotifications
-		Account.findById(req.user.id, function(err, acc){
-			if (err) { res.status(500).json({error:err.message}); return; };
-			
-			Account.update({_id : req.user.id}, {$set : {"deviceId" : req.body.deviceId}}, function(err, result) {
-			    if (err) { 
-			    	res.status(500).json({error:err.message}); 
-			    	return; 
-			    };
-			    if (result) {
-			    	console.log("DeviceId changed!") // everything worked, deviceId changed
-			    }
-			});
+	//*** save the current deviceId for firebase-PushNotifications
+	Account.findById(req.body.userId, function(err, acc){
+		if (err) { res.status(500).json({error:err.message}); return; };
+		
+		Account.update({_id : req.body.userId}, {$set : {"deviceId" : req.body.deviceId}}, function(err, result) {
+		    if (err) { 
+		    	res.status(500).json({error:err.message}); 
+		    	return; 
+		    };
+			if (result) {
+			   	console.log("DeviceId changed!") // everything worked, deviceId changed
+			   	res.status(200).json({"status":"User logged in", "userId": req.user.id, "info" : "deviceId changed successfully"});
+			} else {
+				console.log('deviceId did not change');
+				res.status(401).json({error: 'deviceId change failed.'});
+			}
 		});
-		//*** end
-		res.status(200).json({"status":"User logged in", "userId": req.user.id, "info" : "deviceId changed successfully"});
-	} else {
-		console.log('deviceId did not change');
-		res.status(401).json({error: 'deviceId change failed.'});
-	}
+	});
 });
 
 module.exports = router;
